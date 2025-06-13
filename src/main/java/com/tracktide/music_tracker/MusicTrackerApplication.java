fe651321bf5e11d645765c84a7c836531bd659df
shadeesha <hshadeesha@gmail.com>
2025-06-13 09:17:31 +0100

    init commit

---
+++ src/main/java/com/tracktide/music_tracker/MusicTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracktide.music_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTrackerApplication.class, args);
+	}
+
+}
